7dbcda0de8fb1c11295672675d5ffde6a7ca83d1
jenmaz12 <jenmaz12@gmail.com>
2018-12-13 21:36:52 -0500

    map working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,28 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("googlemaps"), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+  console.log("Hi");
+};
+
 $(document).ready(function(){
     var origin = "";
     var destination = "";
     var key = "AIzaSyCFhkxjfneoXB2xv2kVRkO376AHqTXyqzU";
     var proxy = "https://cors-anywhere.herokuapp.com/";
-$("#button").on("click", function(){
-    origin = $("#start-point").val().trim();
-    destination = $("#destination").val().trim();
-    var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin="+origin+"&destination="+destination+"&key="+key;
+    $("#button").on("click", function(){
+        origin = $("#start-point").val().trim();
+        destination = $("#destination").val().trim();
+        var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin="+origin+"&destination="+destination+"&key="+key;
 
-    $.ajax({
-        url: proxy + queryURL,
-        method: "GET"
-    })
-    .then(function(response){
-        console.log(response);
-    })
-});
-    
+        $.ajax({
+            url: proxy + queryURL,
+            method: "GET"
+        })
+        .then(function(response){
+            console.log(response);
+        })
+    });
 });
