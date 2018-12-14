8950c0a077fd64aa00a971f9353bb4a013cb6503
Ethan Gent <ethan.gent.2391@gmail.com>
2018-12-13 19:59:09 -0500

    got the api talking

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,14 @@ $(document).ready(function(){
     var origin = "";
     var destination = "";
     var key = "AIzaSyCFhkxjfneoXB2xv2kVRkO376AHqTXyqzU";
+    var proxy = "https://cors-anywhere.herokuapp.com/";
 $("#button").on("click", function(){
     origin = $("#start-point").val().trim();
     destination = $("#destination").val().trim();
-    var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&key=" + key;
+    var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin="+origin+"&destination="+destination+"&key="+key;
 
     $.ajax({
-        url: queryURL,
+        url: proxy + queryURL,
         method: "GET"
     })
     .then(function(response){
