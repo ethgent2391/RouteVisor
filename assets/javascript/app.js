acdb9279be915fec72afa6a6673c57cc7530df72
Ethan Gent <ethan.gent.2391@gmail.com>
2018-12-15 12:05:57 -0500

    we got the route!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,33 @@
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById("googlemaps"), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-  console.log("Hi");
-};
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    map = new google.maps.Map(document.getElementById('googlemaps'), {
+      zoom: 7,
+      center: {lat: 41.85, lng: -87.65}
+    });
+    directionsDisplay.setMap(map);
+
+    var onChangeHandler = function() {
+      calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+    document.getElementById('start-point').addEventListener('change', onChangeHandler);
+    document.getElementById('destination').addEventListener('change', onChangeHandler);
+  }
+
+  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: document.getElementById('start-point').value,
+      destination: document.getElementById('destination').value,
+      travelMode: 'DRIVING'
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
 
 $(document).ready(function(){
     var origin = "";
