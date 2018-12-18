a84e8c59c94479afa8163b156220cc91ac90cf44
Web-dev <webdevmac@users.noreply.github.com>
2018-12-17 23:22:18 -0500

    lodges

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,11 @@ var position;
 var marker;
 var responseObject;
 var tripDistanceInput;
+var infowindow = new google.maps.InfoWindow();
+
+var request;
+var service;
+var markers = [];
 
 // Initialize Firebase
 var config = {
@@ -18,16 +23,67 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 function initMap() {
+    var center = new google.maps.LatLng(41.093598, -81.4393721);
     map = new google.maps.Map(document.getElementById('map'), {
         mapTypeControl: false,
         center: { lat: 41.093598, lng: -81.4393721 },
         zoom: 4
 
     });
+    request = {
+        location: center,
+        radius: 4000,
+        types: ['lodging']
+    };
+    service = new google.maps.places.PlacesService(map);
 
+    service.nearbySearch(request, callback);
+
+    google.maps.event.addListener(map, 'rightclick', function (event) {
+        map.setCenter(event.latLng)
+        clearResults(markers)
+
+        var request = {
+            location: event.latLng,
+            radius: 4000,
+            types: ['lodging']
+        };
+        service.nearbySearch(request, callback);
+    })
 
     new AutocompleteDirectionsHandler(map);
 }
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            markers.push(createMarker(results[i]));
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+        infowindow.setContent(place.name);
+    })
+    return marker;
+}
+
+function clearResults(markers) {
+    for (var m in markers) {
+        markers[m].setMap(null)
+    }
+    markers = [];
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
+
 
 function AutocompleteDirectionsHandler(map) {
     this.map = map;
@@ -183,9 +239,24 @@ $("#button").on("click", function () {
         }
 
     }
-    database.ref().push({
-        waypointsArray: waypointsArray
+    waypointsArray.forEach(function (latLng) {
+        service.nearbySearch({
+            location: latLng,
+            radius: 3218,
+            types: ['lodging']
+        }, function (results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    markers.push(createMarker(results[i]));
+                }
+            }
+        });
     });
-       
-    })
+    google.maps.event.addDomListener(window, 'load', initMap);
+    database.ref().push({
+            waypointsArray: waypointsArray
+
+        });
+
+})
 
